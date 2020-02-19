37546fbd35182df75618a55210fd20570f8c21d3
kleotsou <59799713+kleotsou@users.noreply.github.com>
2020-02-20 01:41:15 +0200

    Update exercise12.py

--- exercise12.py
+++ exercise12.py
@@ -18,7 +18,30 @@ if (month == 2):
   if (year%4 == 0) or (year%100 == 0) or (year%400 == 0):
     while (day < 1) or (day > 29) or (day == " "):
       day = int(input("Δώσε την ημέρα που θες:"))
+    print("Ο μήνας έχει 29 μέρες.")
   else:
     while (day < 1) or (day > 28) or (day == " "):
       day = int(input("Δώσε την ημέρα που θες:"))
+    print("Ο μήνας έχει 28 μέρες")
+else:
+  for i in range(1, 9 , 2):
+    if (month == i):
+        while (day < 1) or (day > 31) or (day == " "):
+          day = int( input("Δώσε την ημέρα που θες:"))
+        print("Ο μήνας που δώθηκε έχει 31 μερες.")
+  for i in range(8, 14, 2):
+    if (month == i):
+      while (day < 1) or (day > 31) or (day == " "):
+        day = int( input("Δώσε την ημέρα που θες:"))
+      print("Ο μήνας που δώθηκε έχει 31 μερες.")
+  for i in range(4, 8, 2):
+    if (month == i):
+      while (day < 1) or (day > 30) or (day == " "):
+        day = int( input("Δώσε την ημέρα που θες:"))
+      print("Ο μήνας που δώθηκε έχει 30 μερες.")
+  for i in range(9, 13, 2):
+     if (month == i):
+      while (day < 1) or (day > 30) or (day == " "):
+        day = int( input("Δώσε την ημέρα που θες:"))
+      print("Ο μήνας που δώθηκε έχει 30 μερες.")
     
