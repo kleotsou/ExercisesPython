27eca38d5e4a553426961ee91bf1ec38fd13042c
kleotsou <59799713+kleotsou@users.noreply.github.com>
2020-02-21 14:57:18 +0200

    Update exercise12.py

--- exercise12.py
+++ exercise12.py
@@ -4,16 +4,16 @@ print("Η σημερινή ημερομηνία είναι:", today1)
 
 from datetime import datetime
 now = datetime.now()
-current_time = now.strftime("%M:%S")
+current_time = now.strftime("%H:%M:%S")
 print("Η τρέχουσα ώρα είναι =", current_time)
 
-year = int(input("Δώσε τον χρόνο που θες:")
+year = int(input("Δώσε τον χρόνο που θες:"))
 while (year == " ") or (year < 0):
   year = int(input("Δώσε τον χρόνο που θες:"))
-month = int(input("Δώσε τον μήνα που θες:")
+month = int(input("Δώσε τον μήνα που θες:"))
 while (month < 1) or (month > 12) or (month == " "):
-  month = int(input("Δώσε τον μήνα που θες:")
-day = int(input("Δώσε την ημέρα που θες:")
+  month = int(input("Δώσε τον μήνα που θες:"))
+day = int(input("Δώσε την ημέρα που θες:"))
 if (month == 2):
   if (year%4 == 0) or (year%100 == 0) or (year%400 == 0):
     while (day < 1) or (day > 29) or (day == " "):
