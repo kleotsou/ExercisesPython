7c5ab2d00925da0b6eb468c9513235d094be8905
kleotsou <59799713+kleotsou@users.noreply.github.com>
2020-02-23 16:29:35 +0200

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -4,7 +4,7 @@ def remove_chars(data, chars):
         new_data = new_data.replace(ch, '')
     return new_data
 
-file = open("text.txt", "w")
+file1 = open("text.txt", "w")
 file1.write("Fruits are the means dy which angiosperms disseminate seeds. Edible fruits, in particular, have propagated with the movements of humans and animals in a symbiotic relationship as a means for seed dispersal and nutrition; in fact, humans and many animals have become dependent on fruits as a source of food, Accordingly, fruits account for a substantial fraction of the worlds agricultural output, and some (such as the apple and the pomegranate) have acquired extensive cultural and symbolic meanings. ")                                                                                                                                                                                                                                                                                                                                                                                               
 file1.close()
 file1 = open("text.txt", "r")
@@ -33,7 +33,7 @@ word.sort(key=len)
 lastword = []
 for i in range(x1-1, x1-6, -1):
     m = word[i]
-    z = remove_chas(m, 'aeioyu')
+    z = remove_chars(m, 'aeioyu')
     z1 = len(z)
     new = " "
     for j in range(z1-1, -1, -1):
